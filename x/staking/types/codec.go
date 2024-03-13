0b971921a063e6575ec78482b3ca35a9f7159348
phuongtq <tranphuong.080@gmail.com>
2024-03-13 13:46:46 +0700

    feat(staking): register missing amino codec

--- x/staking/types/codec.go
+++ x/staking/types/codec.go
@@ -23,6 +23,8 @@ func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
 	legacy.RegisterAminoMsg(cdc, &MsgBeginRedelegate{}, "cosmos-sdk/MsgBeginRedelegate")
 	legacy.RegisterAminoMsg(cdc, &MsgCancelUnbondingDelegation{}, "cosmos-sdk/MsgCancelUnbondingDelegation")
 	legacy.RegisterAminoMsg(cdc, &MsgUpdateParams{}, "cosmos-sdk/x/staking/MsgUpdateParams")
+	legacy.RegisterAminoMsg(cdc, &MsgCreateValidatorForOther{}, "cosmos-sdk/MsgCreateValidatorForOther")
+	legacy.RegisterAminoMsg(cdc, &MsgDelegateForOther{}, "cosmos-sdk/MsgDelegateForOther")
 
 	cdc.RegisterInterface((*isStakeAuthorization_Validators)(nil), nil)
 	cdc.RegisterConcrete(&StakeAuthorization_AllowList{}, "cosmos-sdk/StakeAuthorization/AllowList", nil)
